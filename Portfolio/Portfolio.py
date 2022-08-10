9ca70d38db52e63800bcb79a45f0b0bde1258dbd
Benjamin Gottesman Berdah <beni.gottesman@gmail.com>
2022-08-10 17:49:31 +0300

    gv pf

--- Portfolio/Portfolio.py
+++ Portfolio/Portfolio.py
@@ -72,15 +72,20 @@ class State():
     def myStateIs (self) -> None:
         pass
 
+    @abstractmethod
+    def __str__(self): 
+        pass
+
 # The common state interface for all the states
-class LongPortfolio(State):
+class readyToTrade(State):
 
     def entry(self, listInvestments: dict, listQuotation: dict, verbose = False) -> None:
         if verbose:
             print("We entry the strat.")
 
-        if self.pf.BAL <= 0:
-            print("No money in BAL.")
+        tmpBAL = self.pf.getBAL()
+        if tmpBAL <= 0:
+            print("No money in BAL = "+tmpBAL)
             return
 
         for key, quantity in listInvestments.items: #self.pf._children
@@ -89,43 +94,48 @@ class LongPortfolio(State):
                 newShare = Share(key, quantity)
                 self.pf.add(newShare)
             else:
-                child.addQuantity(quantity)
-            self.pf.BAL -= quantity*listQuotation[key]
+                child.addShareQuantity(quantity)
+            tmpBAL -= quantity*listQuotation[key]
 
-    def updateValues(self, listQuotations: dict, verbose = False) -> None:
-        self.pf.TCV = 0
-        for key, value in listQuotations.items: #self.pf._children
-            share = self.pf.getShare(key)
-            self.pf.TCV += share.getQuantity()*value
-        if verbose:
-            print("Portfolio Value "+self.pf.TCV)
+        self.pf.setBAL(tmpBAL)
 
     def exit(self, listQuotation: dict, verbose = False) -> None:
         if verbose:
             print("We exit the strat.")
-        
+
+        tmpBAL = self.pf.getBAL()
         for key, tmp in listQuotation.items: #self.pf._children
             share = self.pf.getLeaf(key)
-            self.pf.BAL += share.getQuantity()*listQuotation[key]
-            self.pf.remove(key)
-        self.pf.TCV = self.pf.BAL
+            tmpBAL += share.getQuantity()*listQuotation[key]
+            share.setQuantity(0) #or we remove
+            # self.pf.remove(key)
+        self.pf.setTCV (tmpBAL)
+        self.pf.setBAL (tmpBAL)
 
+        #ATTENTION CHECK IF IT IS GOOD
+        self.pf.setState(positionClosed())
+
+    @abstractmethod
+    def myStateIs (self) -> None:
+        pass
+
+class LongPortfolio(readyToTrade):
     def myStateIs (self) -> None:
         _type = self._abspf.getType()
         print("The "+_type+" is Long")
 
-class ShortPortfolio (State):
-    def entry(self) -> None:
-        print("we entry the strat")
+    def __str__(self): 
+        return "Long"
 
-    def exit(self) -> None:
-        print("we exit the strat")
-
+class ShortPortfolio(readyToTrade):
     def myStateIs (self) -> None:
         _type = self._abspf.getType()
         print("The "+_type+" is Short")
 
-class Nothing(State):
+    def __str__(self): 
+        return "Short"
+
+class positionClosed(State):
     def entry(self) -> None:
         print("Entry : Actually No Strat")
 
@@ -136,6 +146,9 @@ class Nothing(State):
         _type = self._abspf.getType()
         print("myStateIs : Actually No Strat with "+_type)
 
+    def __str__(self): 
+        return "No Position"
+
 # Leaf
 class Share(AbstractPortfolio):
     _state = None
@@ -145,14 +158,20 @@ class Share(AbstractPortfolio):
         super().__init__(_type)
         self.__name__ = _name #for instance BTCUSDT
         self.setState(state)
-        self.numberOfShares = 0
-        self.BaseCurrentValue = 0
-        self.QuoteCurrentValue = 0
+        self.__numberOfShares__ = 0
+        # self.__BaseCurrentValue__ = 1 because 1 BTC = X Dollar
+        self.__QuoteCurrentValue__ = 0 #for 1 BTC = QuoteCurrentValue $
+
+    #market value
+    def getQuoteCurrentValue (self):
+        return self.__QuoteCurrentValue__
+    def setQuoteCurrentValue (self, value: float):
+        self.__QuoteCurrentValue__ = value
 
     def getQuantity (self):
-        return self.numberOfShares
+        return self.__numberOfShares__
     def setQuantity (self, quantity: float):
-        self.numberOfShares = quantity
+        self.__numberOfShares__ = quantity
 
     def getName (self):
         return self.__name__
@@ -168,19 +187,52 @@ class Share(AbstractPortfolio):
     def getPair(self)-> str:
         return "Pair = "+self.__name__
 
+    #return the value hold in $
     def value(self) -> str:
-        return self.QuoteCurrentValue
+        return self.__QuoteCurrentValue__*self.__numberOfShares__
+
+    def addShareQuantity(self, shareQuantity: float) -> None:
+        self.__numberOfShares__ += shareQuantity
 
 # Composite
 class Portfolio(AbstractPortfolio):
     
     #quoteCurrency="USDT" usually
-    def __init__(self, quoteCurrency: string, state: State, portfolioName: string) -> None:
+    def __init__(self, quoteCurrency: string, portfolioName: string, startingMoney: float) -> None:
         super().__init__("Portfolio")
         self._children: List[AbstractPortfolio] = []
         self.__portfolioName__ = portfolioName
-        self.setState(state)
-        self._quoteCurrency = quoteCurrency
+        self.setState(positionClosed())
+        self.__quoteCurrency__ = quoteCurrency
+        self.__BAL__ = abs(startingMoney)
+        self.__TCV__ = abs(startingMoney)
+
+    def setState(self, state: State) -> None:
+        self.__state__ = state
+
+    #TCV getter setter
+    def getTCV(self) -> float:
+        return self.__TCV__
+    def setTCV(self, value: float) -> None:
+        self.__TCV__ = value
+    def addTCV(self, value: float) -> None:
+        self.__TCV__ += value
+
+    #BAL getter setter
+    def getBAL(self) -> float:
+        return self.__BAL__
+    def setBAL(self, value: float) -> None:
+        self.__BAL__ = value
+    def addBAL(self, value: float) -> None:
+        self.__BAL__ += value
+
+    # operator overloading
+    # add two portfolios
+    def __add__(self, other): 
+        return self.getTCV()+other.getTCV()
+
+    def __str__(self): 
+        return "Value of the portfolio = "+self.__TCV__+self.__quoteCurrency__
 
     def add(self, abspf: AbstractPortfolio) -> None:
         self._children.append(abspf)
@@ -204,13 +256,23 @@ class Portfolio(AbstractPortfolio):
         sys.exit()
 
     def getPortfolioCurrency (self) -> string:
-        return self._quoteCurrency
+        return self.__quoteCurrency__
 
+    #return the TCV, by induction,
+    #it is preferable to updateValues before
     def value(self) -> str:
-        self.TCV = 0
+        self.__TCV__ = 0
         for child in self._children:
-            self.TCV += child.value()
-        return self.TCV
+            self.__TCV__ += child.value()
+        return self.__TCV__
+    
+    #Update the quote value of the pairs
+    def updateValues(self, listQuotations: dict, verbose = False) -> None:
+
+        for child in self._children:
+            for key, value in listQuotations.items: #self.pf._children
+               if child.getName() == key:
+                    child.setQuoteCurrentValue(value)
 
 # if __name__ == "__main__":
 #     tree = Portfolio()
